5b0179ea2836c6e2b2eb2a430c6c8141bf99b139
Feloooooizitas <116197047+Feloooooizitas@users.noreply.github.com>
2023-02-07 18:08:17 +0100

    Optimization

--- Audio_Transmitter/empfaenger.py
+++ Audio_Transmitter/empfaenger.py
@@ -7,79 +7,89 @@ gpio.setup(22,gpio.IN)
 ontimer = 0
 offtimer = 0
 sequence = []
+last20 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
+# Wait until a sound is recieved
+while gpio.input(22) ==0:
+	continue
 
-# Wait until a sound is received
-while True:
-	if gpio.input(22) == 0:
-		break
-
+print("Recieved sound, starting to listen")
 # Listen to inputs
 
-while True:
-	if gpio.input(22) == 0:
-		if offtimer !=0 and len(sequence) !=0:
+while offtimer < 20000:
+		# Make sensor ignore tiny signals
+	last20.append(gpio.input(22))
+	last20.pop(0)
+	if gpio.input(22) == 1 or last20.count(1) >0:	
+		if offtimer !=0 :
 			sequence.append(offtimer)
 		offtimer = 0
 		ontimer += 1
-		print(sequence)
 		
 	else:
 		if ontimer != 0:
 			sequence.append(ontimer)
 		ontimer = 0
 		offtimer +=1
-		print(sequence)
-		# Stop listening
-		if offtimer > sequence[0] * 25:
-			print("Recieving no input")
-			break
-	time.sleep(0.0001)
+	time.sleep(0.0005)
+	
+print("Recieving no input")
 
-print(len(sequence))
 
+print("Recieved input:")
+print(sequence)
+# Remove most of the false inputs
+sequence = [e for e in sequence if e >19]
+
+print("Cleaned input:")
+print(sequence)
 
 # Get approximate length of dit
-speed = (sequence[0]/3 + sequence[1] + sequence[2] + sequence[3] + sequence[4]/3 + sequence[5] + sequence[6] + sequence[7] + sequence[8]/3 + sequence[9]/3) / 10
+speed = (sequence[0]/3 + sequence[1]+ sequence[2] + sequence[3] + sequence[4]/3 + sequence[5]+sequence[6]+sequence[7]+sequence[8]/3+sequence[9]/3)/10
+speed = int(speed)
 
-# Adjust input to speed
+# Remove more short inputs
+sequence = [e for e in sequence if e > speed / 10]
 
-for i in range(10,len(sequence)):
-	if sequence[i] < speed * 1.4:
+# Adjust the inputs to speed
+for i in range(2,len(sequence)):
+	if sequence[i] < speed * 1.7 :
 		sequence[i] = speed
-	elif sequence[i] >= speed * 1.4 and sequence[i] < speed * 5:
+	elif sequence[i] >= speed * 1.7 and sequence[i] < speed * 5:
 		sequence[i] = speed * 3
 	elif sequence[i] >= sequence[0] * 5:
 		sequence[i] = speed * 7
-			
-print(sequence)
-
+		
 # Input lengths are converted into a usable string
 enc = ""
 
-for x in range(10, len(sequence)):
+for x in range(10,len(sequence)):
 	if x % 2 == 0:
 		if sequence[x] == speed:
 			enc += "."
-			print(enc)
 		elif sequence[x] == speed * 3 or sequence[x] == speed * 7:
 			enc += "-"
 	
 	elif x % 2 == 1:
 		if sequence[x] == speed * 3:
 			enc += "/"
-		elif sequence[x] == speed * 7:
+		elif sequence[x] > speed *3:
 			enc += "|"
+print("Translation: \n" + enc)
 
-# Translate
+
+# Translation begins here
 decoded = ""
 letter = ""
-translations = {"-.-.--":"!","|":" ",".-":"A", "-...":"B", "-.-.":"C","-..":"D",".":"E", "..-.":"F","--.":"G","....":"H","..":"I",".---":"J", "-.-":"K", ".-..":"L","--":"M","-.":"N","---":"O",".--.":"P","--.-":"Q",".-.":"R","...":"S","-":"T","..-":"U","...-":"V",".--":"W","-..-":"X","-.--":"Y","--..":"Z",".----":"1","..---":"2","...--":"3","....-":"4",".....":"5","-....":"6","--...":"7","---..":"8","----.":"9","-----":"0",".-.-.-":".","--..--":","}
+translations = {"---...":":","-.-.-.":";","-....-":"-",".-.-":"Ä","...--..":"ß","..--":"Ü","---.":"Ö","..--..":"?","-.-.--":"!","|":" ",".-":"A", "-...":"B", "-.-.":"C","-..":"D",".":"E", "..-.":"F","--.":"G","....":"H","..":"I",".---":"J", "-.-":"K", ".-..":"L","--":"M","-.":"N","---":"O",".--.":"P","--.-":"Q",".-.":"R","...":"S","-":"T","..-":"U","...-":"V",".--":"W","-..-":"X","-.--":"Y","--..":"Z",".----":"1","..---":"2","...--":"3","....-":"4",".....":"5","-....":"6","--...":"7","---..":"8","----.":"9","-----":"0",".-.-.-":".","--..--":","}
 
 for char in enc:
 	if char != "|" and char != "/":
 		letter += char
 	else:
-		decoded += translations[letter]
+		try:
+			decoded += translations[letter]
+		except:
+			print("WARNING! Letter " + letter + " is not identifiable!")
 		letter = ""
 		if char == "|":
 			decoded += " "
